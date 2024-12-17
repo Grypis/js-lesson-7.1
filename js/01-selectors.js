1538378b662615cc54f6bf6db062321310dd9904
grypis <grypisnecros@gmail.com>
2024-12-17 16:16:40 +0200

    soul

---
+++ js/01-selectors.js
@@ -0,0 +1,93 @@
+/* const document1 = {
+  html: {
+    head: {
+      title: {},
+    },
+    body: {
+      p: {
+        a: {},
+      },
+    },
+
+    button: {
+      textContent: '123123',
+      span: {
+        textContent: 'wizard',
+      },
+    },
+  },
+}; */
+
+//! =========================================================
+/* const btnElem = document.querySelector('.js-magic-btn');
+console.log(btnElem.textContent);
+btnElem.textContent = 'Hello world'; */
+
+/* const lastLiEl = document.querySelector('ul.site-nav > li:nth-child(2)');
+console.log(lastLiEl);
+const aElem = lastLiEl.querySelector('a');
+console.log(aElem);
+console.log(aElem.textContent); */
+
+/* const buttonElem = document.querySelector('button.my-btn');
+console.log(buttonElem); */
+
+//! =========================================================
+/* const listElem = document.querySelectorAll('ul a');
+console.log(listElem);
+for (const elem of listElem) {
+  console.log(elem.textContent);
+} */
+
+/* const listElem = document.querySelectorAll('ul a');
+console.log(listElem.length); */
+
+/* const listElem = document.querySelectorAll('.test');
+console.log(listElem); */
+
+//! =========================================================
+//? document.getElementById();
+/* const btnElem = document.getElementById('testBtn');
+console.log(btnElem); */
+
+//? document.getElementsByClassName();
+//? document.getElementsByName();
+/* const listElem = document.getElementsByName('test1');
+console.log(listElem); */
+
+//? document.getElementsByTagName();
+
+//! =========================================================
+// const liElem = document.querySelector('li');
+/* // console.log(liElem);
+const sibling = liElem.nextElementSibling;
+// console.log(sibling);
+const superSibling = liElem.nextElementSibling.nextElementSibling;
+console.log(superSibling); */
+
+/* const previousElem = liElem.previousElementSibling;
+console.log(previousElem); */
+
+/* const parent = liElem.parentElement;
+console.log(parent);
+const grandParent = liElem.parentElement.parentElement;
+console.log(grandParent); */
+
+/* const firstChild = liElem.firstElementChild;
+console.log(firstChild); */
+
+//! =========================================================
+/* const ulElem = document.querySelector('ul');
+const firstChildElem = ulElem.firstElementChild;
+console.log(firstChildElem);
+const lastChildElem = ulElem.lastElementChild;
+console.log(lastChildElem); */
+
+//! =========================================================
+/* const ulElem = document.querySelector('ul');
+const children = ulElem.children;
+console.log(children);
+const secondLiElem = ulElem.children[1];
+console.log(secondLiElem); */
+
+//! =========================================================
