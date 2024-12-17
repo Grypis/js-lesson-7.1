83ed1411799e5e1b3e4501c6fb81a107e7a7f7a2
grypis <grypisnecros@gmail.com>
2024-12-17 20:08:00 +0200

    ol

---
+++ js/02-properties.js
@@ -0,0 +1,73 @@
+//! ========================================================
+/* const image = {
+  src: 'https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=480',
+  alt: "It's me",
+  width: '320px',
+}; */
+
+/* const imageElem = document.querySelector('.js-image');
+console.log(imageElem.src);
+console.log(imageElem.width); */
+
+//! ========================================================
+const imageElem = document.querySelector('.js-image');
+
+//? imageElem.setAttribute('hello', 'Hello world');
+// imageElem.setAttribute('src', 'test');
+// imageElem.setAttribute('height', '300');
+
+//? console.log(imageElem.getAttribute('alt'));
+
+/* const images = document.querySelectorAll('img');
+let count = 0;
+for (const elem of images) {
+  elem.setAttribute(
+    'src',
+    'https://sourse.unsplash.com/1280x720/?random=1&test',
+  );
+}
+ */
+
+//? imageElem.removeAttribute('alt');
+
+//? console.log(imageElem.hasAttribute('disabled'));
+
+//! ========================================================
+/* const createBtn = document.querySelector('button');
+console.log(createBtn); */
+
+// const buttonsElem = document.querySelectorAll('button');
+
+/* console.log(buttonsElem);
+console.log(buttonsElem[0].getAttribute('data-action'));
+console.log(buttonsElem[1].getAttribute('data-action'));
+console.log(buttonsElem[2].getAttribute('data-action')); */
+
+/* const btnElem = buttonsElem[0];
+console.log(btnElem.dataset);
+console.log(btnElem.dataset.action);
+console.log(btnElem.dataset.id);
+console.log(btnElem.dataset.text);
+
+btnElem.dataset.value = 'hello'; */
+
+//! ========================================================
+/* const containerElem = document.querySelector('.js-test-container');
+console.log(containerElem);
+
+const pElem = containerElem.children[0];
+const btnElem = containerElem.children[1];
+const inputElem = containerElem.children[2]; */
+
+/* console.log(pElem.textContent);
+console.log(btnElem.textContent);
+console.log(inputElem.textContent); */
+
+/* pElem.textContent = 'test';
+btnElem.textContent = 'test';
+inputElem.textContent = 'test'; */
+
+/* console.log(inputElem.value);
+inputElem.value = 'test'; */
+
+// inputElem.setAttribute('placeholder', 'TEST PLACEHOLDER');
